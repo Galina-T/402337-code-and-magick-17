37b5b8d7499c02d2b40289a78bcae62533c207d8
Galina-T <kazarinagg@gmail.com>
2019-06-11 03:25:25 +0300

    Вносим правки

--- js/setup.js
+++ js/setup.js
@@ -14,31 +14,57 @@ var COAT_COLORS = [
 
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-function rand(arr) {
-  return Math.floor(Math.random() * arr.length);
-}
-
-function getWizards(quantity) {
-  var arr = [];
-  for (var i = 0; i < quantity; i++) {
-    var wizard = {};
-    wizard.name = WIZARD_NAMES[rand(WIZARD_NAMES)];
-    wizard.surname = WIZARD_SURNAMES[rand(WIZARD_SURNAMES)];
-    wizard.coatColor = COAT_COLORS[rand(COAT_COLORS)];
-    wizard.eyesColor = EYES_COLORS[rand(EYES_COLORS)];
-    arr.push(wizard);
-  }
-  return arr;
-}
-
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
+/**
+* Возвращает целое случайное число в диапазоне [min; max]
+*
+* @param {number} min минимальное значение
+* @param {number} max максимальное значение
+* @return {number} случайное число.
+*/
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+* Генерирует объект, описывающий мага
+*
+* @return {object} обьект, описывающий мага.
+*/
+function getWizard() {
+  var wizard = {};
+  wizard.name = WIZARD_NAMES[getRandomNumber(0, WIZARD_NAMES.length - 1)];
+  wizard.surname = WIZARD_SURNAMES[getRandomNumber(0, WIZARD_SURNAMES.length - 1)];
+  wizard.coatColor = COAT_COLORS[getRandomNumber(0, COAT_COLORS.length - 1)];
+  wizard.eyesColor = EYES_COLORS[getRandomNumber(0, EYES_COLORS.length - 1)];
+  return wizard;
+}
+
+/**
+* Генерирует массив нескольких магов
+*
+* @param {number} quantity количество магов для генерации
+* @return {object} массив из n сгенерированных объектов, описывающих магов.
+*/
+function getWizards(quantity) {
+  var arr = [];
+  for (var i = 0; i < quantity; i++) {
+    arr.push(getWizard());
+  }
+  return arr;
+}
+
+/**
+* Подготавливает DOM Node объект мага
+*
+* @param {object} wizard объект мага, сгенерированного `getWizard`
+* @return {object} DOM Node мага
+*/
 function renderWizard(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -49,14 +75,32 @@ function renderWizard(wizard) {
   return wizardElement;
 }
 
-var wizards = getWizards(4);
+/**
+* Отрисовывает магов
+*
+* @param {number} quantity количество магов для отрисовки
+*/
+function showWizards(quantity) {
+  var wizards = getWizards(quantity);
+  var fragment = document.createDocumentFragment();
 
-var fragment = document.createDocumentFragment();
-
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
 }
 
-similarListElement.appendChild(fragment);
+/**
+* Удаляет класс у DOM элемента
+*
+* @param {object} root родительский DOM элемент
+* @param {string} el селектор элемента, у которого удаляется класс
+* @param {string} className имя класса, который необходимо удалить
+*/
+function setup(root, el, className) {
+  root.querySelector(el).classList.remove(className);
+}
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+setup(document, '.setup', 'hidden');
+showWizards(4);
+setup(userDialog, '.setup-similar', 'hidden');
